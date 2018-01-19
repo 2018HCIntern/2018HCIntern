9e9116e53ceae02761dd22bfad5e9356fbe2f17b
Giyeon Kim <gompu123@gmail.com>
2018-01-19 10:48:52 +0900

    gan paper, mnist generator

---
+++ GAN/milestone.mnist/cnn.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+# CNN
+class CNN(nn.Module):
+    # constructor
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_dropout = nn.Dropout()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    # forward method
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_dropout(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x)
---
+++ GAN/milestone.mnist/mnist.py
@@ -0,0 +1,32 @@
+from __future__ import print_function
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.autograd import Variable
+
+"""
+1. MNIST TRAINING
+"""
+
+
+"""
+2. GAN MNIST
+"""
+
+
+"""
+3. LABELED DATA GAN MNIST
+"""
+
+
+"""
+4. MNIST WITH GENERATED DATA
+"""
+
+
+"""
+5.
+"""
